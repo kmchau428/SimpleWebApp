5fac133ab2dc3338d96de252a45c972165c63663
kmchau <kmchau428@gmail.com>
2016-01-18 15:08:14 +0000

    added steve jobs

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -8,6 +8,10 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        else if (query.toLowerCase().contains("steve jobs")) {
+            return "Apple Inc. founder!";
+        }
+        
         return "";
     }
 }
