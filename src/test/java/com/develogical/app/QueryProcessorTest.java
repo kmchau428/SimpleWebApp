7a2fc5ab05f3fe48a27290e239bb8c0785814d45
kmchau <kmchau428@gmail.com>
2016-01-21 15:21:38 +0000

    commit-3

--- src/test/java/com/develogical/app/QueryProcessorTest.java
+++ src/test/java/com/develogical/app/QueryProcessorTest.java
@@ -10,23 +10,6 @@ public class QueryProcessorTest {
 
     QueryProcessor queryProcessor = new QueryProcessor();
 
-    @Test
-    public void knowsAboutShakespeare() throws Exception {
-        assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
-    }
-
-    @Test
-    public void isNotCaseSensitive() throws Exception {
-        assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
-    }
+  
     
-    @Test
-    public void knowsAboutSteve() throws Exception {
-        assertThat(queryProcessor.process("Steve Jobs"), containsString("Apple"));
-    }
-
-    @Test
-    public void isNotCaseSensitiveSteve() throws Exception {
-        assertThat(queryProcessor.process("steve jobs"), containsString("Apple"));
-    }
 }
